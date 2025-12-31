b5849f2fb40cdf8b38ffbd091df2822613dac12e
stty88 <stty.88vn@gmail.com>
2025-12-31 16:37:45 +0700

    a

--- script.js
+++ script.js
@@ -151,7 +151,7 @@ document.addEventListener("DOMContentLoaded", function () {
     function draw() {
       // clear
       var img = new Image();
-      img.src = "image/tet.jpg";
+      img.src = "./tet.jpg";
   
       img.onload = function () {
         ctx.globalCompositeOperation = "source-over";
